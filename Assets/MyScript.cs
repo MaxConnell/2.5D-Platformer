af737fc4b1b5e957f8fdb76b8c5b854e03d5373e
BuildTools <unconfigured@null.spigotmc.org>
2017-06-10 17:11:33 +0100

    Added something

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -6,6 +6,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("Hello world.");
+		print ("Something has changed.");
 	}
 	
 	// Update is called once per frame
